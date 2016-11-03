7240f64123ee56cef64ef6b6044f6f80ad63cb3c
NinoCo <ninocoaz@gmail.com>
2016-11-03 11:02:09 +0100

    Create rednotouch_lv2.js

---
+++ javascript/rednotouch_lv2.js
@@ -0,0 +1,129 @@
+var g, keys, gameObjects;
+
+function init() {
+  var canvas, i, obj, loop;
+  canvas = document.getElementsByTagName("canvas")[0];
+  g = canvas.getContext("2d");
+  
+  keys = [];
+  addEventListener("keydown", function(e){
+    keys[e.keyCode] = true;
+  });
+  addEventListener("keyup", function(e){
+    keys[e.keyCode] = false;
+  });
+
+  gameObjects = [];
+  for (i=0;i<map.obstacles.length;i++) {
+    obj = map.obstacles[i];
+    gameObjects.push(new GameObject(obj[0], obj[1], obj[2], obj[3], obj[4]));
+  }
+  for (i=0;i<map.players.length;i++) {
+    obj = map.players[i];
+    gameObjects.push(new GameObject(obj[0], obj[1], obj[2], obj[3], "green", playerUpdate));
+  }
+
+  loop = function() {
+    update();
+    render();
+    requestAnimationFrame(loop);
+  };
+  loop();
+}
+
+function update() {
+  var i, gameObject;
+  for(i=0;i<gameObjects.length;i++) {
+    gameObject = gameObjects[i];
+    gameObject.update();
+  }
+}
+
+function render() {
+  var i, gameObject;
+  g.clearRect(0, 0, g.canvas.width, g.canvas.height);
+  for(i=0;i<gameObjects.length;i++) {
+    gameObject = gameObjects[i];
+    gameObject.render();
+  }
+}
+
+
+function GameObject(x, y, width, height, color, _update) {
+  this.x = this.startX = x;
+  this.y = this.startY = y;
+  this.width = width;
+  this.height = height;
+  this.color = color;
+  this.speedX = 0;
+  this.speedY = 0;
+  this._update = _update && _update.bind(this);
+  
+  this.update = function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    if (this._update) this._update();
+  }
+  
+  this.render = function() {
+    g.fillStyle = this.color;
+    g.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function playerUpdate(){
+  this.speedX = 0;
+  this.speedY = 0;
+  if (keys[37]) { this.speedX = -2; }
+  if (keys[39]) { this.speedX = 2; }
+  if (keys[38]) { this.speedY = -2; }
+  if (keys[40]) { this.speedY = 2; }
+
+var other = detectCollision(this);
+if (other) {
+  if (other.color === "yellow") {
+    alert("You win!");
+    window.location.reload();
+  } else {
+    this.x = this.startX;
+    this.y = this.startY;
+  }
+}
+}
+
+function detectCollision(myObject) {
+  var i, gameObject,
+      myLeft, myRight, myTop, myBottom,
+      otherLeft, otherRight, otherTop, otherBottom;
+  
+  myLeft = myObject.x;
+  myRight = myObject.x + myObject.width;
+  myTop = myObject.y;
+  myBottom = myObject.y + myObject.height;
+  
+  for(i=0;i<gameObjects.length;i++) {
+    gameObject = gameObjects[i];
+    if (gameObject !== myObject) {
+      otherLeft = gameObject.x;
+      otherRight = gameObject.x + gameObject.width;
+      otherTop = gameObject.y;
+      otherBottom = gameObject.y + gameObject.height;
+      if ((myBottom > otherTop) &&
+          (myTop < otherBottom) &&
+          (myRight > otherLeft) &&
+          (myLeft < otherRight)) {
+        return gameObject;
+      }
+    }
+  }
+}
+
+var map = {
+  "obstacles": [
+    // x, y, width, height
+
+  ],
+  "players": [
+
+  ]
+};
